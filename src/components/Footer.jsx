bfb7de8a13c19ba329cba3eac8cc0413ae065416
Alexis Gonzalez <alexis.gonzalez.390@gmail.com>
2026-06-18 23:09:31 -0700

    Adding favicon and footer img

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,25 @@
+import ghost from "../assets/SEA-Icon-1x1.png";
 export default function Footer() {
   return (
     <footer>
-      compiled by hand, not by webpack &middot; <span>good luck next week</span>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <img
+          style={{
+            width: "50px",
+            height: "50px",
+            alignItems: "center",
+            paddingRight: "15px",
+          }}
+          src={ghost}
+        />
+        <span style={{ alignItems: "center" }}>good luck next week</span>{" "}
+      </div>
     </footer>
   );
 }
